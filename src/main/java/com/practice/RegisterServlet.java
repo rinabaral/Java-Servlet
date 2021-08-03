e0d75aae0b414f6487846d2895a5a48c4d820ab2
Rina Baral <rina12baral@gmail.com>
2021-08-03 22:51:28 +0545

    first servlet

---
+++ src/main/java/com/practice/RegisterServlet.java
@@ -0,0 +1,36 @@
+package com.practice;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+
+public class RegisterServlet extends HttpServlet {
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+	{
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("<h2>Welcome to Register Servlet</h2>");
+		String name = request.getParameter("user_name");
+		String password = request.getParameter("user_password");
+		String email = request.getParameter("user_email");
+		String gender = request.getParameter("user_gender");
+		String course = request.getParameter("user_course");
+		
+		String cond = request.getParameter("condition");
+		
+		if(cond.equals("checked")) {
+			out.println("<h2> Name: " + name + "</h2>");
+			out.println("<h2> Password: " + password  + "</h2>");
+			out.println("<h2> Email: " +email  + "</h2>");
+			out.println("<h2> Gender: " +gender  + "</h2>");
+			out.println("<h2> Course: " + course + "</h2>");
+			
+		}else {
+			out.println("<h1>You have not accepted terms and conditions</h2>");
+		}
+		
+	
+	}
+}
